987fe2e3d17667551c5c478fb1010597eee479ae
David Harper <david.harper@dreamportsoftware.com>
2018-01-31 09:13:44 +0000

    Building Vue SPA Laravel Part 1

---
+++ resources/assets/js/views/Hello.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <p>Hello World!</p>
+        <img src="https://www.microsoft.com/en-gb/CMSImages/WindowsHello_Poster_1920-1600x300-hello.png?version=0d8f51c7-ef87-b0af-8f26-453fb40b4b7d"/>
+    </div>
+</template>
